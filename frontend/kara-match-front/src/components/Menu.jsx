2177f6d9505c5a86d214fd614ae52bd31b09a329
OKGO-007 <86036181+OKGO-007@users.noreply.github.com>
2023-02-28 15:23:43 +0900

    Update Menu.jsx
    adjust

--- frontend/kara-match-front/src/components/Menu.jsx
+++ frontend/kara-match-front/src/components/Menu.jsx
@@ -149,6 +149,7 @@ const Menu = (props) => {
         </Grid>
 
         <Grid item xs={4}>
+          <h1 className="sample-box-02">Search Users</h1>
           <div>
             <Search/>
           </div>
